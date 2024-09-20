b81b9d4d048590a11180b0b4a64068b63342b626
DrowAnn <rs19990801@gmail.com>
2024-09-20 14:25:31 -0500

    adicion de ruta para actualizacion de usuarios

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Patch } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
+import { ActualizarJornadaLaboralDto } from 'src/jornadas-laborales/dto/actualizar-jornada-laboral.dto';
 
 @Controller('usuarios')
 export class UsuariosController {
   constructor(private readonly usuariosService: UsuariosService) {}
+
+  @Patch(':nombreUsuario')
+  actualizacionParcial(
+    @Param('nombreUsuario') nombreUsuario: string,
+    @Body() data: ActualizarJornadaLaboralDto,
+  ) {
+    return this.usuariosService.actualizarUsuario(nombreUsuario, data);
+  }
 }
